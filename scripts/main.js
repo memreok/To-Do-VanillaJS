209eac9999bd42faef0c22b92a1f6a19797d4d8e
Mehmet Emre Ök <mehmetemreok2@gmail.com>
2026-04-06 20:17:29 +0300

    search bar eklendi

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,20 @@
+let itemsId = 0
+
+
+
 function AddTask(){
+  itemsId += 1
   alert("Fonksiyon tetiklendi!")
+}
+
+function DeleteTask(){
+  itemsId -=1
+}
+
+function EditTask(){
+
+}
+
+function SearchTask(){
+  
 }
