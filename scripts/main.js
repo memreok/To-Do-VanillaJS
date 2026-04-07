781de696b94f88306a075506c473276d1685aeb2
Mehmet Emre Ök <mehmetemreok2@gmail.com>
2026-04-07 21:59:41 +0300

    input alma ve frontend düzeltmeleri

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,61 @@
 let itemsId = 0
 
+let taskStr = "Default Text";
 
+const searchBtn = document.getElementById("searchBtn");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("list");
+
+addBtn.addEventListener("click" , e => {
+
+    
+  console.log("görev ekleniyor...");
+  const item = document.createElement("li");
+  const input = document.createElement("input");
+  
+  
+  item.appendChild(input);
+  list.appendChild(item);
+  addEventListener("keydown", e => {
+    if(e.key === "Enter" && (input.value !== "")){
+
+
+      let task = document.createElement("p");
+      taskStr = input.value;
+      task = taskStr;
+      
+
+
+      
+      item.removeChild(input);
+      
+
+      item.append(task);
+      
+      itemsId += 1; 
+      console.log(task + " " + itemsId);
+
+
+    }
+
+
+
+  })
+  
+
+
+})
+
+
+
+searchBtn.addEventListener("click", e => {
+  console.log("görev aranıyor...");
+})
 
 function AddTask(){
   itemsId += 1
-  alert("Fonksiyon tetiklendi!")
+  alert("çalıştım aga");
+
 }
 
 function DeleteTask(){
@@ -16,5 +67,5 @@ function EditTask(){
 }
 
 function SearchTask(){
-  
+  console.log("aradım aga");
 }
